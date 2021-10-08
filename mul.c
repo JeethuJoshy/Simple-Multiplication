a312dde3c61631559a3bc769031bd80050769593
Jeethu Joshy <56990091+JeethuJoshy@users.noreply.github.com>
2021-10-08 11:40:44 +0530

    Add files via upload

---
+++ mul.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+void main()
+{
+	int a,b,mul;
+	printf("Enter the first number:");
+	scanf("%d",&a);
+	printf("Enter the second number:");
+	scanf("%d",&b);
+	mul=a*b;
+	printf("multiplication:%d\n",mul);
+}
